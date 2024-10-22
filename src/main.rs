692de118d6c9398447be401ae45f6ac77ce5c717
shirayoru <130097435+shirayoru@users.noreply.github.com>
2024-10-23 00:12:33 +0800

    add epub, html5ever for reading epub and parsing xhtml

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,16 @@
 use std::fs;
 
-use lexist::common::SfTokenizer;
+use lexist::tokenizer::SfTokenizer;
 use sudachi::prelude::Mode;
+
+use epub::doc::EpubDoc;
 
 fn main() {
 
-    let data = fs::read_to_string("resources/input.txt").unwrap();
+    let doc= EpubDoc::new("resources/epub/epub3-spec.epub").unwrap();
+
+    doc.get_num_pages();
 
     let mut tokenizer = SfTokenizer::new_built(Mode::C);
 
-    tokenizer.tokenize(&data).iter().for_each(|m| {
-        println!("{:?}", m);
-    });
 }
